bcdb5dc50eefbcc2ae096e279811696f07d3bebc
Nick van der Worp - WEB <nick.vanderworp@one-web.nl>
2024-11-11 17:10:18 +0100

    models add

--- Base/urls.py
+++ Base/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from Home.views import Home_view
 from About.views import About_view
 from Contact.views import Contact_view
-from Blog.views import Blog_view
 
 urlpatterns = [
     path('', Home_view, name='base_view'),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),  
     path('About/', About_view, name='about_view'),
     path('Contact/', Contact_view, name='contact_view'),
-    path('Blog/', Blog_view, name='blog_view'),
+    path('Blog/', include('Blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Blog/views.py
+++ Blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render 
+from .models import MyModel
 
 def Blog_view(request): 
-    return render(request, "blog_view.html") 
+    mymodels = MyModel.objects.all()
+    return render(request, 'blog_view.html', {'mymodels': mymodels})
