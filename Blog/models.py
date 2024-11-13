51a0b3e2ac5a3b6a87c10bf6e6e98e723e7b9360
Nick van der Worp - WEB <nick.vanderworp@one-web.nl>
2024-11-13 09:20:56 +0100

    change the models

--- Blog/models.py
+++ Blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class MyModel(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    date = models.TextField()
     image = models.ImageField(upload_to='images/')
     
     def __str__(self):
